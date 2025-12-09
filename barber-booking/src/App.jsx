a5739c970604ec87b75b1c6fea0c88db4d11e8ed
nikhil.ko <nikhil.ko@poornam>
2025-12-09 21:17:57 +0530

    added image

--- barber-booking/src/App.jsx
+++ barber-booking/src/App.jsx
@@ -1,11 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 
-// pages
 import HomePage from "./pages/HomePage";
 import ServicesPage from "./pages/ServicesPage";
 import BarbersPage from "./pages/BarbersPage";
-import DateTimePage from "./pages/DateTimePage.jsx";
+import DateTimePage from "./pages/DateTimePage";
 import CustomerForm from "./pages/CustomerForm";
 import ConfirmationPage from "./pages/ConfirmationPage";
 
@@ -15,17 +14,23 @@ function App() {
   return (
     <BrowserRouter>
       <div className="app-bg">
-        <NavBar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/services" element={<ServicesPage />} />
-            <Route path="/barbers" element={<BarbersPage />} />
-            <Route path="/booking" element={<DateTimePage />} />
-            <Route path="/customer" element={<CustomerForm />} />
-            <Route path="/confirmation" element={<ConfirmationPage />} />
-          </Routes>
+        <div className="overlay"></div>   {/* blur layer */}
+
+        <div className="content-wrapper">
+          <NavBar />
+
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/barbers" element={<BarbersPage />} />
+              <Route path="/booking" element={<DateTimePage />} />
+              <Route path="/customer" element={<CustomerForm />} />
+              <Route path="/confirmation" element={<ConfirmationPage />} />
+            </Routes>
+          </div>
         </div>
+
       </div>
     </BrowserRouter>
   );
