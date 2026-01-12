7e293f73047e1306d4ca6d5c347dacff298c6661
nikhil <nikhil.ko@poornam.com>
2026-01-12 23:21:10 +0530

    Entry pages changes

--- barber-booking/src/App.jsx
+++ barber-booking/src/App.jsx
@@ -1,42 +1,42 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import NavBar from "./components/NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
-// import HomePage from "./pages/HomePage";
-// import ServicesPage from "./pages/ServicesPage";
-// import BarbersPage from "./pages/BarbersPage";
-// import DateTimePage from "./pages/DateTimePage";
-// import CustomerForm from "./pages/CustomerForm";
-// import ConfirmationPage from "./pages/ConfirmationPage";
+import HomePage from "./pages/HomePage";
+import ServicesPage from "./pages/ServicesPage";
+import BarbersPage from "./pages/BarbersPage";
+import DateTimePage from "./pages/DateTimePage";
+import CustomerForm from "./pages/CustomerForm";
+import ConfirmationPage from "./pages/ConfirmationPage";
 
-// import "./App.css";
+import "./App.css";
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <div className="app-bg">
-//         <div className="overlay"></div>   {/* blur layer */}
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-bg">
+        <div className="overlay"></div>   {/* blur layer */}
                    
-//         <NavBar />  {/* stays fixed at top, NOT inside centering box */}
-//         <div className="content-wrapper">
+        <NavBar />  {/* stays fixed at top, NOT inside centering box */}
+        <div className="content-wrapper">
 
-//           <div className="content">
-//             <Routes>
-//               <Route path="/" element={<HomePage />} />
-//               <Route path="/services" element={<ServicesPage />} />
-//               <Route path="/barbers" element={<BarbersPage />} />
-//               <Route path="/booking" element={<DateTimePage />} />
-//               <Route path="/customer" element={<CustomerForm />} />
-//               <Route path="/confirmation" element={<ConfirmationPage />} />
-//             </Routes>
-//           </div>
-//         </div>
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/barbers" element={<BarbersPage />} />
+              <Route path="/booking" element={<DateTimePage />} />
+              <Route path="/customer" element={<CustomerForm />} />
+              <Route path="/confirmation" element={<ConfirmationPage />} />
+            </Routes>
+          </div>
+        </div>
 
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
+      </div>
+    </BrowserRouter>
+  );
+}
 
-// export default App;
+export default App;
 
 
 import HomePage from "./pages/HomePage";
